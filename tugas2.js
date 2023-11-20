79531a2485fdbda33c7ec98fcf7fcaebbb068e26
sonny faristrianto <sonnyf4r15@gmail.com>
2023-11-20 21:07:16 +0700

    add tugas2

---
+++ tugas2.js
@@ -0,0 +1,11 @@
+let umur = 39;
+let nama = "Sonny Farisrianto";
+let wni = true;
+let pelanggaran = null;
+let hobby = ["catur", "gambar", "games", "touring"];
+
+console.log(nama);
+console.log(umur);
+console.log(wni);
+console.log(pelanggaran);
+console.log(hobby);
